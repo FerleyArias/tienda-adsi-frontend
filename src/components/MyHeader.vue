c3c16b3f5b8f77a6f7b564c5fdc2cbf81c28b8f3
Ferley Arias <ferley225@gmail.com>
2021-04-22 07:21:02 -0500

    fix header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,19 +1,33 @@
 <template>
   <header class="flex justify-between bg-blue-700 text-white p-5">
     <div class="flex items-center">
-      <button class="mr-3 ">
+      <button v-show="visible" class="mr-3 ">
         <font-awesome-icon icon="bars"/>
       </button>
       <h3 class="text-lg">Bodega</h3>
     </div>
-    <button>
+    <button v-show="visible">
         <font-awesome-icon class="text-2xl" icon="sign-out-alt"/>
     </button>
   </header>
 </template>
 
 <script>
+import {ref, watch} from 'vue'
+import {useRoute} from 'vue-router'
+
 export default {
   name: 'MyHeader',
+  setup() {
+    const route = useRoute()
+    const visible = ref(false)
+
+    watch(route, ()=>{
+      visible.value = !(route.path === "/login")
+    })
+    return{
+      visible
+    }
+  }
 }
 </script>
