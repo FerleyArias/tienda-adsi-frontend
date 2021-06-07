e4bbd7fcb9bec9ddcb15775d78eff43ef1d92588
EdwinJPA <ejpaez1@misena.edu.co>
2021-06-07 13:42:03 -0500

    Por si las cago

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="flex justify-between bg-blue-700 text-white p-5 absolute w-full bottom-0">
-    <h3>©Analisis y Desarrollo de Sistemas de Informacion</h3>
+    <h3 class="message">©Analisis y Desarrollo de Sistemas de Informacion</h3>
   </footer>
 </template>
 
@@ -9,3 +9,9 @@ export default {
   name: 'MyFooter',
 }
 </script>
+
+<style scoped>
+.message {
+    margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <main class="statistics">
+    <canvas id="myChart" width="70vw" height="75vh"></canvas>
+    {{sales}}
+  </main>
 </template>
 <script>
+import { useStore } from "vuex";
+import { computed, onMounted } from "vue";
 export default {
   name: "Home",
   setup() {
+    const store = useStore();
+    const sales = computed(() => store.state.ventas);
+    const getSales = () => store.dispatch("getVenta");
+    onMounted(() => {
+      getSales();
+    });
 
+    return {
+      getSales,
+      sales,
+    };
   },
 };
 </script>
+<style scoped>
+.statistics {
+  margin-left: 25vw;
+  width: 70vw;
+  height: 75vh;
+  background-color: blue;
+}
+</style>
