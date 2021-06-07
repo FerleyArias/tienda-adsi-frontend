5ffaf28a8798f7d6b01b9a00a4d1a7d077ef2b97
EdwinJPA <ejpaez1@misena.edu.co>
2021-06-07 14:33:00 -0500

    Agregando la tabla de estadísticas con vue chart 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <main class="statistics">
-    <canvas id="myChart" width="70vw" height="75vh"></canvas>
-    {{sales}}
+      <Doughnut class="Doughnut" :data="testData" />
   </main>
 </template>
 <script>
 import { useStore } from "vuex";
-import { computed, onMounted } from "vue";
+import { ref, computed, onMounted } from "vue";
+import { Doughnut } from "vue-chart-3";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 export default {
   name: "Home",
+  components: { Doughnut },
   setup() {
     const store = useStore();
     const sales = computed(() => store.state.ventas);
     const getSales = () => store.dispatch("getVenta");
+
+    const getSalesByMonth = (sales) => {
+        console.log(sales)
+    }
+
+    getSalesByMonth(sales)
+
+    const dataValues = ref([30, 40, 60, 70, 5, 30, 40, 60, 70, 5, 12 ,2]);
+    const testData = computed(() => ({
+      labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+      datasets: [
+        {
+          data: dataValues.value,
+          backgroundColor: [
+            "#FFF042", //1
+            "#FFA32F", //2
+            "#F46D21", //3
+            "#F4322B", //4
+            "#EA0045", //5
+            "#BB0064", //6
+            "#6D0284", //7
+            "#4516A2", //8
+            "#0235A5", //9
+            "#008AAC", //10
+            "#00985C", //11
+            "#60AA38", //12
+          ],
+        },
+      ],
+    }));
+
+
     onMounted(() => {
       getSales();
     });
@@ -20,15 +56,21 @@ export default {
     return {
       getSales,
       sales,
+      testData
     };
   },
 };
 </script>
 <style scoped>
 .statistics {
-  margin-left: 25vw;
-  width: 70vw;
-  height: 75vh;
-  background-color: blue;
+  margin: 0 auto;
+  width: 40%;
+  height: 40%;
+}
+
+.Doughnut {
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
 }
 </style>
